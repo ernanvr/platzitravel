ac074866fda04aec4a0c3cbb1bbdf907a964b619
ernanvr <ernanvr@gmail.com>
2022-04-11 23:22:06 -0600

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import * as React from 'react';
+
+const Home: NextPage = () => {
+  return (
+    <h1 className='text-3xl font-bold underline '>
+      Hello world!
+    </h1>
+  );
+};
+
+export default Home;
